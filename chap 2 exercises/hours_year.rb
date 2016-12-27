2d2f6617ce82bedfd3e405213e4492604f22705c
mnguyen1450 <mnguyen1450@berkeley.edu>
2016-12-27 00:40:47 -0800

    changed file names

---
+++ chap 2 exercises/hours_year.rb
@@ -0,0 +1,2 @@
+num = 24*365
+puts "there are " + num.to_s + " hours in a year"
--- test.rb
+++
@@ -1,4 +0,0 @@
-puts 'hello, what is your name?'
-name = gets.chomp
-puts 'Your name is ' + name +'? what a lovely name!'
-puts 'nice to meet you ' + name + '.  :)'
